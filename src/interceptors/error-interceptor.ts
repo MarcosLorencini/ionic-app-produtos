bc2fdc2ffdb25fb87108ff94e83fd45e308b7e89
Marcos Lorencini <mlorencinif@gmail.com>
2019-09-12 17:27:33 -0300

    Tratamento padrao para outros erros  com Alert

--- src/interceptors/error-interceptor.ts
+++ src/interceptors/error-interceptor.ts
@@ -2,12 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpEvent, HttpInterceptor, HttpHandler, HttpRequest, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { Observable } from 'rxjs/Rx'; // IMPORTANTE: IMPORT ATUALIZADO
 import { StorageService } from '../sevices/storage.service';
+import { AlertController } from 'ionic-angular/components/alert/alert-controller';
 
 //classe que intercepta os erros
 @Injectable()
 export class ErrorInterceptor implements HttpInterceptor {
 
-    constructor(public storage: StorageService){}
+    constructor(public storage: StorageService, public alertCtrl : AlertController){}
 
     //intercepta a requisicao que é realizada no app e aplica a logica que esta no método
     intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
@@ -27,9 +28,16 @@ export class ErrorInterceptor implements HttpInterceptor {
 
             //tratar os erros
             switch(errorObj.status) {
+                case 401:
+                this.handle401();
+                break;
+
                 case 403:
                 this.handle403();
                 break;
+
+                default:
+                this.handleDefaultError(errorObj);
             }
 
             //propaga o erro para o contrololador
@@ -37,10 +45,46 @@ export class ErrorInterceptor implements HttpInterceptor {
         }) as any;
     }
 
+    handle401() {
+        //chama o alert
+        let alert = this.alertCtrl.create({
+            title: 'Erro 401: falha de autenticação',
+            message: 'Email ou senha incorretos',
+            //tem que cliar no alert para fechar
+            enableBackdropDismiss: false,
+            buttons: [
+                {
+                    text: 'Ok'
+                }
+
+            ]
+        });
+        alert.present();
+    }
+
     //trata o erro 403
     //remove o objeto do localStorage
     handle403() {
         this.storage.setLocalUser(null);
+    }
+
+    handleDefaultError(errorObj){
+    //erro qualquer
+    let alert = this.alertCtrl.create({
+        title: 'Erro '+errorObj.status + ':' +errorObj.error,
+        message: errorObj.message,
+        //tem que cliar no alert para fechar
+        enableBackdropDismiss: false,
+        buttons: [
+            {
+                text: 'Ok'
+            }
+
+        ]
+    });
+    alert.present();
+
+
     }
 }
 
